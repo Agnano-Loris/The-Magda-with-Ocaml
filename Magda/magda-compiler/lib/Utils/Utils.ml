7ce92754b2495c97032eccb932523e52223e20d4
Francesco De Matteis <de_matteis.francesco@educ.di.unito.it>
2026-05-13 23:17:04 +0200

    Updated with Cst

--- Magda/magda-compiler/lib/Utils/Utils.ml
+++ Magda/magda-compiler/lib/Utils/Utils.ml
@@ -1,3 +1,4 @@
 module StringSet = Set.Make(String)
 module StringMap = Map.Make(String)
 module MagdaType = MagdaType
+module Cst = Cst
--- Magda/magda-compiler/lib/Utils/Utils.mli
+++ Magda/magda-compiler/lib/Utils/Utils.mli
@@ -1,3 +1,4 @@
 module StringSet : Set.S with type elt = string
 module StringMap : Map.S with type key = string
 module MagdaType = MagdaType
+module Cst = Cst 
