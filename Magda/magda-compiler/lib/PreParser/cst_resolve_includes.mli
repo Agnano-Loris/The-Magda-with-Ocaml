42a58e2c8492e76eadbe6522b0b3199d055a0771
Francesco De Matteis <de_matteis.francesco@educ.di.unito.it>
2026-08-11 21:58:45 +0200

    minor changes to documentation

--- Magda/magda-compiler/lib/PreParser/cst_resolve_includes.mli
+++ Magda/magda-compiler/lib/PreParser/cst_resolve_includes.mli
@@ -3,14 +3,15 @@ type resolved_program = resolved_global_declaration list
     Files are included at most once; subsequent includes of the same file produce an empty program.
 *)
 
-and resolved_global_declaration =
-| DeclarationMixin of Utils.Cst.mixin_declaration
-| DeclarationLet of Utils.Cst.let_declaration
-| DeclarationInclude of string * resolved_program
 (** Extended global declaration type, represents the basic declarations and the unfolded include declaration.
     [DeclarationMixin] and [DeclarationLet] wrap the corresponding CST type, [DeclarationInclude (filename, program)] contains
     the filename of a magda file and the recursively resolved program of the included file.
 *)
+and resolved_global_declaration =
+| DeclarationMixin of Utils.Cst.mixin_declaration
+| DeclarationLet of Utils.Cst.let_declaration
+| DeclarationInclude of string * resolved_program
+
 
 val resolve_includes : Utils.Cst.program -> string -> resolved_program
 (** [resolve_includes cst filename] resolves all include declarations in [cst], starting from [filename].
